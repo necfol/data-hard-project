bbea97c21c68c82d6f3548dd03f0a54451bbb77b
necfol <necfoll@gmail.com>
2016-03-27 20:48:37 +0800

    注册测试

--- routes/signup.js
+++ routes/signup.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var dbapi = require('../conf/dbapi');
 /* GET users listing. */
 router.post('/', function(req, res) {
-	var r = dbapi.add({
-		name: 'Necfol',
-		phone: '13811111111'
-	});
-	console.error('出结果啦')
-	console.error(r);
+	// var r = dbapi.add('user', {
+	// 	username: 'Necfol',
+	// 	sex: 1,
+	// 	createtime: 1111111111111,
+	// 	phone: 13811111111
+	// });
 	// console.error(req.body)
 	res.json('ok');
 });
