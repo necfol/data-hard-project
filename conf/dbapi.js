4a1b8e6cb2788e96518de52982b5e0bfd71ac956
necfol <necfoll@gmail.com>
2016-03-30 23:40:57 +0800

    暂存，这么写不对，明天写mysql连接池

--- conf/dbapi.js
+++ conf/dbapi.js
@@ -1,30 +1,43 @@
 var mysql = require('mysql');
 var dbconf = require('./conf.db.js');
 var dbapi = {};
-dbapi.add = function(tabel, post,cb) {
-		var connection = mysql.createConnection({
-			host: dbconf.connection.host,
-			user: dbconf.connection.user,
-			port: 8889,
-			password: dbconf.connection.password,
-			database: dbconf.database
-		});
-		connection.connect();
-		var query = 'INSERT INTO ' + tabel + ' SET ?';
-		connection.query(query, post, cb);
-		connection.end();
-	}
-dbapi.selectuser = function(tabel, id , columns, post, cb) {
-		var connection = mysql.createConnection({
-			host: dbconf.connection.host,
-			user: dbconf.connection.user,
-			port: 8889,
-			password: dbconf.connection.password,
-			database: dbconf.database
-		});
-		connection.connect();
-		var query = 'SELECT ?? FROM ?? WHERE ' +	id + ' = ?';
-		connection.query(query, [columns, tabel, post], cb);
-		connection.end();
-	}
+dbapi.add = function(tabel, post, cb) {
+	var connection = mysql.createConnection({
+		host: dbconf.connection.host,
+		user: dbconf.connection.user,
+		port: 8889,
+		password: dbconf.connection.password,
+		database: dbconf.database
+	});
+	connection.connect();
+	var query = 'INSERT INTO ' + tabel + ' SET ?';
+	connection.query(query, post, cb);
+	connection.end();
+}
+dbapi.selectuser = function(tabel, id, columns, post, cb) {
+	var connection = mysql.createConnection({
+		host: dbconf.connection.host,
+		user: dbconf.connection.user,
+		port: 8889,
+		password: dbconf.connection.password,
+		database: dbconf.database
+	});
+	connection.connect();
+	var query = 'SELECT ?? FROM ?? WHERE ' + id + ' = ?';
+	connection.query(query, [columns, tabel, post], cb);
+	connection.end();
+}
+dbapi.login = function(tabel, columns, username, password, cb) {
+	var connection = mysql.createConnection({
+		host: dbconf.connection.host,
+		user: dbconf.connection.user,
+		port: 8889,
+		password: dbconf.connection.password,
+		database: dbconf.database
+	});
+	connection.connect();
+	var query = 'SELECT * FROM user WHERE username = "' + username + '" AND password = "' + password + '"';
+	var ss = connection.query(query, cb);
+	connection.end();
+}
 module.exports = dbapi;
